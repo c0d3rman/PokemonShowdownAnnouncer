2d7f536c15d79637c949c6dbe5aa92f04192a121
Yoni Lerner <yonilern@gmail.com>
2021-08-19 12:38:11 -0700

    Multi-room support that maybe works

--- inject.js
+++ inject.js
@@ -1,5 +1,3 @@
-let roomID = undefined;
-
 // Setup a listener to receive messages from the content script
 let assetMap;
 window.postMessage({
@@ -22,18 +20,16 @@ window.addEventListener("message", (event) => {
 	}
 }, false);
 
-window.app._focusRoom_orig = window.app.focusRoom;
-window.app.focusRoom = function(id, focusTextbox) {
-	// TODO: handle room change
-
-	window.app._focusRoom_orig(id, focusTextbox);
-}
-
-room.battle.scene._teamPreview_orig = room.battle.scene.teamPreview;
-room.battle.scene.teamPreview = function() {
-	soundManager.play("battle_start");
-
-	room.battle.scene._teamPreview_orig();
+let soundManager = {
+	play: function(name, type="sound") {
+		window.postMessage({
+			type: "SHOWDOWN_ANNOUNCER",
+			source: "PAGE",
+			content: "PLAY_SOUND",
+			soundName: name,
+			soundType: type
+		}, "*");
+	}
 }
 
 // Hijack the mute function so we can mute too
@@ -47,161 +43,169 @@ let sendMuteMessage = function(muted) {
 };
 window.BattleSound._setMute_orig = window.BattleSound.setMute;
 window.BattleSound.setMute = function(muted) {
-	sendMuteMessage(muted);
+	sendMuteMessage(muted); 
 	window.BattleSound._setMute_orig(muted);
 };
 sendMuteMessage(window.BattleSound.muted); // Send initial mute data
 
-let soundManager = {
-	play: function(name, type="sound") {
-		window.postMessage({
-			type: "SHOWDOWN_ANNOUNCER",
-			source: "PAGE",
-			content: "PLAY_SOUND",
-			soundName: name,
-			soundType: type
-		}, "*");
+rooms = [];
+
+setInterval(function() {
+	if (!(window.room && window.room.battle && !rooms.includes(window.room.id))) {
+		return;
 	}
-}
 
-let dataHandler = {
-	handle: function(data) {
-		let lines = data.split("\n");
-		if (lines[0] == "|init|battle") {
-			// This is a catch-up message, ignore
-			return;
-		}
-		for (let line of lines) {
-			this.processLine(line);
-		}
-	},
-	processLine: function(line) {
-		params = line.slice(1).split("|");
-		command = params[0];
-		params.shift();
+	rooms.push(window.room.id);
+	let room = window.room;
 
-		switch (command) {
-			case "move":
-				let move = params[1];
-				if (assetMap.moves.includes(move)) {
-					soundManager.play(move, "move");
-				}
-				break;
-			case "switch":
-				let pokemon = params[1].split(", ")[0];
-				if (assetMap.pokemon.includes(pokemon)) {
-					soundManager.play(pokemon, "pokemon");
-				} else {
-					soundManager.play("switch");
-				}
-				break;
-			case "drag":
-				soundManager.play("switch_forced");
-				break;
-			case "-miss":
-				// TODO: repeat misses / multi misses
-				soundManager.play("miss");
-				break;
-			case "-weather":
-				if (params[1] == "[upkeep]") {
+	room.battle.scene._teamPreview_orig = room.battle.scene.teamPreview;
+	room.battle.scene.teamPreview = function() {
+		soundManager.play("battle_start");
+
+		room.battle.scene._teamPreview_orig();
+		// Never do it again after the first time
+		room.battle.scene.teamPreview = room.battle.scene._teamPreview_orig;
+	}
+
+	let dataHandler = {
+		handle: function(data) {
+			let lines = data.split("\n");
+			if (lines[0] == "|init|battle") {
+				// This is a catch-up message, ignore
+				return;
+			}
+			for (let line of lines) {
+				this.processLine(line);
+			}
+		},
+		processLine: function(line) {
+			params = line.slice(1).split("|");
+			command = params[0];
+			params.shift();
+
+			switch (command) {
+				case "move":
+					let move = params[1];
+					if (assetMap.moves.includes(move)) {
+						soundManager.play(move, "move");
+					}
 					break;
-				}
-				switch (params[0]) {
-					case "Sandstorm":
-						soundManager.play("sand_start");
+				case "switch":
+					let pokemon = params[1].split(", ")[0];
+					if (assetMap.pokemon.includes(pokemon)) {
+						soundManager.play(pokemon, "pokemon");
+					} else {
+						soundManager.play("switch");
+					}
+					break;
+				case "drag":
+					soundManager.play("switch_forced");
+					break;
+				case "-miss":
+					// TODO: repeat misses / multi misses
+					soundManager.play("miss");
+					break;
+				case "-weather":
+					if (params[1] == "[upkeep]") {
 						break;
-					case "RainDance":
-						soundManager.play("rain_start");
-						break;
-					case "SunnyDay":
-						soundManager.play("sun_start");
-						break;
-					case "none":
-						switch (rooms[currentRoom].weather) {
-							case "Sandstorm":
-								soundManager.play("sand_end");
+					}
+					switch (params[0]) {
+						case "Sandstorm":
+							soundManager.play("sand_start");
+							break;
+						case "RainDance":
+							soundManager.play("rain_start");
+							break;
+						case "SunnyDay":
+							soundManager.play("sun_start");
+							break;
+						case "none":
+							switch (rooms[currentRoom].weather) {
+								case "Sandstorm":
+									soundManager.play("sand_end");
+									break;
+								case "RainDance":
+									soundManager.play("rain_end");
+									break;
+								case "SunnyDay":
+									soundManager.play("sun_end");
+									break;
+							}
+
+					}
+					rooms[currentRoom].weather = params[0];
+					break;
+				case "-fail":
+					soundManager.play("fail");
+					break;
+				case "-activate":
+					if (params[1].startsWith("move: ")) {
+						let move = params[1].split(": ")[1];
+						switch (move) {
+							case "Protect":
+							case "Detect":
+							case "Baneful Bunker":
+							case "Crafty Shield":
+							case "King's Shield":
+							case "Mat Block":
+							case "Obstruct":
+							case "Quick Guard":
+							case "Spiky Shield":
+							case "Wide Guard":
+							case "Max Guard":
+								soundManager.play("blocked");
 								break;
-							case "RainDance":
-								soundManager.play("rain_end");
-								break;
-							case "SunnyDay":
-								soundManager.play("sun_end");
+							case "Substitute":
+								soundManager.play("sub_hit");
 								break;
 						}
-
-				}
-				rooms[currentRoom].weather = params[0];
-				break;
-			case "-fail":
-				soundManager.play("fail");
-				break;
-			case "-activate":
-				if (params[1].startsWith("move: ")) {
-					let move = params[1].split(": ")[1];
-					switch (move) {
-						case "Protect":
-						case "Detect":
-						case "Baneful Bunker":
-						case "Crafty Shield":
-						case "King's Shield":
-						case "Mat Block":
-						case "Obstruct":
-						case "Quick Guard":
-						case "Spiky Shield":
-						case "Wide Guard":
-						case "Max Guard":
-							soundManager.play("blocked");
+					}
+					break;
+				case "-start":
+					switch (params[1]) {
+						case "Substitute":
+							soundManager.play("sub_start");
+							break;
+					}
+					break;
+				case "-end":
+					switch (params[1]) {
+						case "Disable":
+							soundManager.play("disable_end");
 							break;
 						case "Substitute":
 							soundManager.play("sub_hit");
 							break;
 					}
-				}
-				break;
-			case "-start":
-				switch (params[1]) {
-					case "Substitute":
-						soundManager.play("sub_start");
-						break;
-				}
-				break;
-			case "-end":
-				switch (params[1]) {
-					case "Disable":
-						soundManager.play("disable_end");
-						break;
-					case "Substitute":
-						soundManager.play("sub_hit");
-						break;
-				}
-				break;
-			case "-enditem":
-				let item = params[1];
-				let reason = params[2];
-				if (reason == "[eat]") {
-					// TODO
-				}
-				break;
-			case "-immune":
-				soundManager.play("no_effect");
-				break;
-			case "-crit":
-				soundManager.play("crit");
-				break;
-			case "-supereffective":
-				soundManager.play("supereffective");
-				break;
-			case "-resisted":
-				soundManager.play("not_very_effective");
-		}
-	},
-}
+					break;
+				case "-enditem":
+					let item = params[1];
+					let reason = params[2];
+					if (reason == "[eat]") {
+						// TODO
+					}
+					break;
+				case "-immune":
+					soundManager.play("no_effect");
+					break;
+				case "-crit":
+					soundManager.play("crit");
+					break;
+				case "-supereffective":
+					soundManager.play("supereffective");
+					break;
+				case "-resisted":
+					soundManager.play("not_very_effective");
+			}
+		},
+	}
 
-// Hijack the function that receives Sim Protocol messages from the server
-window.room.receive = function(data) {
-	// Handle the data
-	dataHandler.handle(data)
-	
-	// Make sure the room gets the data too
-	this.add(data);
-};
+	// Hijack the function that receives Sim Protocol messages from the server
+	room.receive = function(data) {
+		// Handle the data
+		dataHandler.handle(data)
+		
+		// Make sure the room gets the data too
+		this.add(data);
+	};
+}, 100);
